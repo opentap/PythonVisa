67e9784b74192c6090dc0fa58279c2bf091040f8
Alan Copeland <alan_copeland@keysight.com>
2023-01-31 18:20:16 -0700

    Current state, unit tests are not running yet because of an error.

--- unittest/UnitTest1.cs
+++ unittest/UnitTest1.cs
@@ -1,15 +1,33 @@
-namespace unittest;
+using NUnit.Framework;
+using OpenTap.Plugins.BasicSteps;
+using OpenTap;
+using System.IO;
+using System;
 
-public class Tests
+namespace unittest 
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+            System.IO.Directory.CreateDirectory("Settings/Bench/Default");
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            System.IO.File.Copy("../../bin/Debug/Packages/Python/PythonVisa/test/PythonVisa.xml", "Settings/PythonVisa.xml", true);
+            System.IO.File.Copy("../../bin/Debug/Packages/Python/PythonVisa/test/Instruments.xml", "Settings/Bench/Default/Instruments.xml", true);
+            System.IO.File.Copy("../../bin/Debug/Packages/Python/PythonVisa/test/simulation_instrument.yaml", "simulation_instrument.yaml", true);
+        }
+
+        [Test]
+        public void SimulatedIdnTest()
+        {
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            var plan = OpenTap.TestPlan.Load("../../bin/Debug/Packages/Python/PythonVisa/test/idn.TapPlan");
+            var run = plan.Execute();
+            
+            
+            Assert.AreEqual(Verdict.Pass, run.Verdict);
+        }
+
     }
 }
